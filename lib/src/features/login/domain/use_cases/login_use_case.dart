f5cdbdf971820e33c23b941a1baba288ffe82bd0
HenriqueHartmann <alemaohartmann@gmail.com>
2023-09-03 20:57:12 -0300

    Add LoginUseCase

---
+++ lib/src/features/login/domain/use_cases/login_use_case.dart
@@ -0,0 +1,33 @@
+import 'package:auto_login_app/src/core/use_cases/use_case.dart';
+import 'package:auto_login_app/src/features/login/data/models/auth_model.dart';
+import 'package:auto_login_app/src/features/login/data/models/user_model.dart';
+import 'package:auto_login_app/src/features/login/domain/repositories/auth_repository.dart';
+
+class LoginUseCase extends UseCase<UserModel, LoginParams> {
+  final AuthRepository repository;
+
+  LoginUseCase(this.repository);
+
+  @override
+  Future<UserModel> execute(LoginParams params) async {
+    final body = AuthModel(login: params.login, password: params.password);
+
+    final response = await repository.authenticate(body: body);
+
+    // TODO: Save Login
+
+    return response;
+  }
+}
+
+class LoginParams {
+  final String login;
+  final String password;
+  final bool rememberMe;
+
+  LoginParams({
+    required this.login,
+    required this.password,
+    this.rememberMe = false,
+  });
+}
