889d77350a5788cc98e2b606d1902e4b221985c1
HenriqueHartmann <alemaohartmann@gmail.com>
2023-09-08 02:38:07 -0300

    Update dependency injection

--- lib/src/features/login/domain/di/login_di.dart
+++ lib/src/features/login/domain/di/login_di.dart
@@ -1,7 +1,9 @@
+import 'package:auto_login_app/src/features/login/data/data_sources/auth_local_data_source.dart';
 import 'package:auto_login_app/src/features/login/data/data_sources/auth_remote_data_source.dart';
 import 'package:auto_login_app/src/features/login/data/repositories/auth_repository_impl.dart';
 import 'package:auto_login_app/src/features/login/domain/repositories/auth_repository.dart';
 import 'package:auto_login_app/src/features/login/domain/use_cases/login_use_case.dart';
+import 'package:auto_login_app/src/features/login/domain/use_cases/save_login_use_case.dart';
 import 'package:auto_login_app/src/features/login/presentation/cubit/login_cubit.dart';
 import 'package:get_it/get_it.dart';
 
@@ -11,16 +13,19 @@ class LoginDI {
     instance.registerFactory(
       () => LoginCubit(
         loginUseCase: instance(),
+        saveLoginUseCase: instance(),
       ),
     );
 
     // Use cases
     instance.registerLazySingleton(() => LoginUseCase(instance()));
+    instance.registerLazySingleton(() => SaveLoginUseCase(instance()));
 
     // Repository
     instance.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(
         remoteDataSource: instance(),
+        localDataSource: instance(),
         networkInfo: instance(),
       ),
     );
@@ -28,6 +33,9 @@ class LoginDI {
     // Data sources
     instance.registerLazySingleton<AuthRemoteDataSource>(
       () => AuthRemoteDataSourceImpl(client: instance()),
+    );
+    instance.registerLazySingleton<AuthLocalDataSource>(
+      () => AuthLocalDataSourceImpl(storageService: instance()),
     );
   }
 
