fd72a1e6689319b33f5b031e4f6dcacb447703f1
HenriqueHartmann <alemaohartmann@gmail.com>
2023-09-10 20:00:39 -0300

    Add New Failures classes

--- lib/src/core/error/failure.dart
+++ lib/src/core/error/failure.dart
@@ -1,3 +1,9 @@
 abstract class Failure {}
 
 class SerializerFailure extends Failure {}
+
+class SecureStorageEmptyFailure extends Failure {}
+
+class AuthenticationFailure extends Failure {}
+
+class ResponseFailure extends Failure {}
