b6e4eee086e60af03c5b24d4d5970c5cff98234d
HenriqueHartmann <alemaohartmann@gmail.com>
2023-09-10 21:20:19 -0300

    Update dependency injection

--- lib/src/core/config/injection_config.dart
+++ lib/src/core/config/injection_config.dart
@@ -1,5 +1,9 @@
 import 'package:auto_login_app/src/core/network/network_info.dart';
 import 'package:auto_login_app/src/core/services/secure_storage_service.dart';
+import 'package:auto_login_app/src/core/shared/data/data_sources/stored_login_local_data_source.dart';
+import 'package:auto_login_app/src/core/shared/data/repositories/stored_login_repository_impl.dart';
+import 'package:auto_login_app/src/core/shared/domain/repositories/stored_login_repository.dart';
+import 'package:auto_login_app/src/core/shared/domain/use_cases/clean_stored_login_use_case.dart';
 import 'package:auto_login_app/src/features/login/domain/di/login_di.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
@@ -12,6 +16,21 @@ class InjectionConfig {
     sl.registerLazySingleton(() => http.Client());
     sl.registerLazySingleton<SecureStorageService>(
         () => SecureStorageService());
+
+    // Use cases
+    sl.registerLazySingleton(() => CleanStoredLoginUseCase(sl()));
+
+    // Repository
+    sl.registerLazySingleton<StoredLoginRepository>(
+      () => StoredLoginRepositoryImpl(
+        localDataSource: sl(),
+      ),
+    );
+
+    // Data sources
+    sl.registerLazySingleton<StoredLoginLocalDataSource>(
+      () => StoredLoginLocalDataSourceImpl(storageService: sl()),
+    );
 
     LoginDI().init(instance: sl);
   }
--- lib/src/features/login/domain/di/login_di.dart
+++ lib/src/features/login/domain/di/login_di.dart
@@ -1,7 +1,7 @@
-import 'package:auto_login_app/src/features/login/data/data_sources/auth_local_data_source.dart';
 import 'package:auto_login_app/src/features/login/data/data_sources/auth_remote_data_source.dart';
 import 'package:auto_login_app/src/features/login/data/repositories/auth_repository_impl.dart';
 import 'package:auto_login_app/src/features/login/domain/repositories/auth_repository.dart';
+import 'package:auto_login_app/src/features/login/domain/use_cases/get_stored_login_use_case.dart';
 import 'package:auto_login_app/src/features/login/domain/use_cases/login_use_case.dart';
 import 'package:auto_login_app/src/features/login/domain/use_cases/save_login_use_case.dart';
 import 'package:auto_login_app/src/features/login/presentation/cubit/login_cubit.dart';
@@ -14,18 +14,20 @@ class LoginDI {
       () => LoginCubit(
         loginUseCase: instance(),
         saveLoginUseCase: instance(),
+        getStoredLoginUseCase: instance(),
+        cleanStoredLoginUseCase: instance(),
       ),
     );
 
     // Use cases
     instance.registerLazySingleton(() => LoginUseCase(instance()));
     instance.registerLazySingleton(() => SaveLoginUseCase(instance()));
+    instance.registerLazySingleton(() => GetStoredLoginUseCase(instance()));
 
     // Repository
     instance.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(
         remoteDataSource: instance(),
-        localDataSource: instance(),
         networkInfo: instance(),
       ),
     );
@@ -33,9 +35,6 @@ class LoginDI {
     // Data sources
     instance.registerLazySingleton<AuthRemoteDataSource>(
       () => AuthRemoteDataSourceImpl(client: instance()),
-    );
-    instance.registerLazySingleton<AuthLocalDataSource>(
-      () => AuthLocalDataSourceImpl(storageService: instance()),
     );
   }
 
